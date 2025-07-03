13c04c99900c82dae964805e5264dec81f7d5b69
TechG <133663470+TechG2@users.noreply.github.com>
2025-07-03 07:23:53 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const passport = require("passport");
 const flash = require("express-flash");
 const session = require("express-session");
